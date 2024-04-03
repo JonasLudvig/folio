f46fb7c34a3d20ea5ca70caa0160f51488347806
JonasLudvig <89127328+JonasLudvig@users.noreply.github.com>
2024-04-03 20:58:35 +0200

    Update content.

--- src/components/pages/home/service/Service.tsx
+++ src/components/pages/home/service/Service.tsx
@@ -8,6 +8,10 @@ export default function Service() {
         <div className='service-item about-item-first-js'>
           <h4>Frontend</h4>
           <p>Interface development using modern techniques and frameworks.</p>
+          <span>
+            Streamlined and well considered methodology using frameworks like
+            React.js, TypeScript and Sass.
+          </span>
         </div>
         <div className='service-image'>
           <img src={stack} alt='Stack' />
@@ -15,24 +19,44 @@ export default function Service() {
         <div className='service-item about-item-second-js'>
           <h4>Backend</h4>
           <p>Sustainable and secure user platform and utility development.</p>
+          <span>
+            Java, C# and PHP with focus on maintainability and best practice in
+            regards to configuratin and design.
+          </span>
         </div>
         <div className='service-item about-item-third-js'>
           <h4>Cloud</h4>
           <p>
             Database and server configuration, maintenance and optimization.
           </p>
+          <span>
+            Development utilizing techniques and cloud services like Apache HTTP
+            Server, Microsoft Azure, SQL Server, MongoDb, MySQL, SQLite.
+          </span>
         </div>
         <div className='service-item about-item-forth-js'>
           <h4>Media</h4>
           <p>Standardized type, image and video media production.</p>
+          <span>
+            Technical orientation toward producing graphic content using vectors
+            and bitmaps.
+          </span>
         </div>
-        <div className='service-item about-item-forth-js'>
+        <div className='service-item about-item-fifth-js'>
           <h4>Design</h4>
           <p>Web interface and graphic profile development.</p>
+          <span>
+            Prototyping and preliminary studies with a practical approach
+            focusing on implementation and results.
+          </span>
         </div>
-        <div className='service-item about-item-forth-js'>
+        <div className='service-item about-item-sixth-js'>
           <h4>Requisite</h4>
           <p>Conceptualization and requisites analysis.</p>
+          <span>
+            Organizing and concretization with emphasis on feasibility,
+            performance and delivery.
+          </span>
         </div>
       </div>
     </div>
